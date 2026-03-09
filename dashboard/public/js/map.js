a0b341209c4dc6ec280b68fe9eeadc12f0bca53b
aledegro <belliorum@gmail.com>
2026-03-09 10:38:41 +0100

    small tweak

--- dashboard/public/js/map.js
+++ dashboard/public/js/map.js
@@ -21,6 +21,7 @@ const MapView = {
       center: [50.85, 4.35], // Brussels
       zoom: 4,
       minZoom: 2,
+      maxZoom: 7,
       maxBounds: [[-85, -180], [85, 180]],
       maxBoundsViscosity: 1.0,
       zoomControl: false,
@@ -31,7 +32,7 @@ const MapView = {
     L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OSM</a> &copy; <a href="https://carto.com/">CARTO</a>',
       subdomains: 'abcd',
-      maxZoom: 19,
+      maxZoom: 7,
     }).addTo(this.map);
 
     // Marker layer group
